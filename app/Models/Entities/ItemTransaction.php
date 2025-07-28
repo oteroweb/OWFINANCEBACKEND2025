d6ea1cd8275bf0095d8c29801de1b59832de2f2d
Jose Otero (Oteroweb) <oterolopez1990@gmail.com>
2025-07-27 22:12:31 -0400

    Add ItemTransaction entity

---
+++ app/Models/Entities/ItemTransaction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models\Entities;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ItemTransaction extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'transaction_id',
+        'name',
+        'amount',
+        'tax_id',
+        'rate_id',
+        'description',
+        'jar_id',
+        'active',
+        'deleted_at',
+        'date',
+        'category_id',
+        'user_id',
+        'custom_name',
+    ];
+
+    protected $casts = [
+        'date'       => 'datetime:Y-m-d H:i:s',
+        'created_at' => 'datetime:Y-m-d',
+        'updated_at' => 'datetime:Y-m-d',
+        'deleted_at' => 'datetime:Y-m-d',
+    ];
+
+    protected static function newFactory()
+    {
+        return \Database\Factories\ItemTransactionFactory::new();
+    }
+}
